a5b568f47989c763349d11a69548aec6032a79a4
Michal Mitter <mittermichal@gmail.com>
2016-09-26 17:14:48 +0000

    return ics file

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ flask_app = Flask(__name__)
 @flask_app.route('/cal/<regex>')
 def render_get(regex):
   getCal(regex)
-  return send_from_directory(filename='my.ics')
+  return send_from_directory(filename='my.ics',directory='.')
 
 if __name__ == "__main__":
-  flask_app.run(port=5999, host='0.0.0.0')
+  flask_app.run(port=5998, host='0.0.0.0', debug=True)
