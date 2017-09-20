ee2423c679cd81ab4dfe8f57ceaaace8b5659818
Michal Mitter <mittermichal@gmail.com>
2017-09-20 12:33:20 +0200

    cache headers

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, make_response
 from banda_parser import getCal
 
 flask_app = Flask(__name__)
@@ -6,7 +6,9 @@ flask_app = Flask(__name__)
 @flask_app.route('/cal/<regex>')
 def render_get(regex):
   getCal(regex)
-  return send_from_directory(filename='my.ics',directory='.')
-
+  resp = make_response(send_from_directory(filename='my.ics',directory='.'))
+  resp.cache_control.max_age = 300
+  resp.cache_control.public = True
+  return resp
 if __name__ == "__main__":
   flask_app.run(port=5998, host='0.0.0.0', debug=True)
